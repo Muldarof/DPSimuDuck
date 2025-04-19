9bbbbc913d72e3f233c04f7cb7809888584a902e
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-04-19 17:55:22 -0400

    refactored mallard duck and model duck to use constructors to set class variables

--- MallardDuck.java
+++ MallardDuck.java
@@ -2,9 +2,12 @@ import FlyBehavior.FlyWithWings;
 import QuackBehavior.Quack;
 
 public class MallardDuck extends Duck {
-    flyBehavior = new FlyWithWings();
-    quackBehavior = new Quack();
 
+
+    public MallardDuck(){
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+    }
 
     public void display(){
         System.out.println("I'm a real Mallard Duck");
--- ModelDuck.java
+++ ModelDuck.java
@@ -2,8 +2,11 @@ import FlyBehavior.FlyNoWay;
 import QuackBehavior.Quack;
 
 public class ModelDuck extends Duck{
-    flyBehavior = new FlyNoWay();
-    quackBehavior = new Quack();
+
+    public ModelDuck(){
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
 
     public void display(){
         System.out.println("I'm a model duck");
