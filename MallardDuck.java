06fc5de735d548eed60913eecbbb0616068013df
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-04-19 17:23:45 -0400

    tidying up in the mallard ducck class

--- MallardDuck.java
+++ MallardDuck.java
@@ -2,8 +2,9 @@ import FlyBehavior.FlyWithWings;
 import QuackBehavior.Quack;
 
 public class MallardDuck extends Duck {
-    quackBehavior = new Quack();
     flyBehavior = new FlyWithWings();
+    quackBehavior = new Quack();
+
 
     public void display(){
         System.out.println("I'm a real Mallard Duck");
